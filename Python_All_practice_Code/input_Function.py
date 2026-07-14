295bba43d8817f14e05a678276116a4f3a225ba1
VivekGoswami017 <vivekgoswamivg336278@gmail.com>
2026-07-14 08:17:24 +0530

    Create input_Function.py

---
+++ Python_All_practice_Code/input_Function.py
@@ -0,0 +1,10 @@
+name =input("Enter your name: ")
+age =int(input("Enter your age: "))
+print(name)
+print(age)
+
+
+student_name =input("enter a student name : ")
+student_age =input("enter a student age : ")
+student_email =input("enter a valid email :")
+print(student_name,student_age,student_email)
