f41544b8cc4e35c890991628e8095b93ae3cb1b7
VivekGoswami017 <vivekgoswamivg336278@gmail.com>
2026-07-14 08:17:19 +0530

    Create DataTypes.py

---
+++ Python_All_practice_Code/DataTypes.py
@@ -0,0 +1,23 @@
+Name="Python"
+print(Name,"--->",type(Name))
+
+Age=21
+print(Age,"--->",type(Age))
+
+Tech=['Python','java','Sql']
+print(Tech,"--->",type(Tech))
+
+C_no=(12345,67890)
+print(C_no,"-->",type(C_no))
+
+Data={'Address' : "Nikol"}
+print(Data,"-->",type(Data))
+
+Salary=123.456
+print(Salary,"--->",type(Salary))
+
+Is_worker=True
+print(Is_worker,"--->",type(Is_worker))
+
+Id={101,102,103}
+print(Id,"--->",type(Id))
