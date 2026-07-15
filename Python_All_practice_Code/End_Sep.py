879e3b7a66b149c6c12a0c877ca50fc2a495f6f7
VivekGoswami017 <vivekgoswamivg336278@gmail.com>
2026-07-15 08:21:05 +0530

    Create End_Sep.py

---
+++ Python_All_practice_Code/End_Sep.py
@@ -0,0 +1,16 @@
+#end Parameter
+print("Python", end=" -> ")
+print("Data Science")
+
+
+print("Hello", end="\n")
+print("World")
+
+
+# sep Parameter
+print("Python", "SQL", "Excel", sep=" | ")
+print("2026", "07", "12", sep="-")
+
+
+#sep + end
+print("Python", "SQL", "Excel", sep=" | ", end=" <-- Skills")
