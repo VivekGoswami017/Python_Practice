3bbdad8544b01e39513dcae608f5f4d42b3872cb
VivekGoswami017 <vivekgoswamivg336278@gmail.com>
2026-07-15 08:19:58 +0530

    Create Typecasting.py

---
+++ Python_All_practice_Code/Typecasting.py
@@ -0,0 +1,16 @@
+# Convert string to integer
+num_str ="10"
+num_int =int(num_str)
+print(type(num_int)) 
+
+
+# Convert integer to string
+num =25
+num_str =str(num)
+print(type(num_str)) 
+
+
+# Convert float to integer
+pi =3.14
+pi_int =int(pi)
+print(type(pi_int))
