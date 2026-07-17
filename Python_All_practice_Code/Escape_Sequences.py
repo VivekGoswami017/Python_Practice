3659054a3a2ef522431fee0236eafe73004a3e5a
VivekGoswami017 <vivekgoswamivg336278@gmail.com>
2026-07-17 08:49:07 +0530

    Create Escape_Sequences.py

---
+++ Python_All_practice_Code/Escape_Sequences.py
@@ -0,0 +1,11 @@
+print("Hello\nWorld!")
+
+print("This is a tab\tcharacter.")
+
+print("c:\\Users\\vivek\\Desktop")
+
+print("He said, \"python is Easy\"")
+
+print('It\'s a beautiful day !')
+
+print("ABC\bD")
