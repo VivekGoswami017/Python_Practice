1743e374d240c1a7906b4a9aa9d57d510283dbb0
VivekGoswami017 <vivekgoswamivg336278@gmail.com>
2026-07-18 08:59:42 +0530

    Create if-elif-else Statement.py

---
+++ Python_All_practice_Code/Conditional Statements/if-elif-else Statement.py
@@ -0,0 +1,10 @@
+Age=int(input("enter Age :"))
+
+if Age>=18 and Age<=110:
+    print("User is Eligible For Vote")
+
+elif Age>=1 and Age<=17:
+    print("User is Not Eligible For Voting")
+
+else:
+    print("Enterd Age is Invalid...")
