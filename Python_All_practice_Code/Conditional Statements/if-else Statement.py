8d589401ce43d6528990fd760cc17ff517dec28a
VivekGoswami017 <vivekgoswamivg336278@gmail.com>
2026-07-18 08:59:38 +0530

    Create if-else Statement.py

---
+++ Python_All_practice_Code/Conditional Statements/if-else Statement.py
@@ -0,0 +1,8 @@
+Number = int(input("Enter a Number :"))
+
+if Number > 0:
+    print(f"{Number} is a Positive Number.")
+else:
+    print(f"{Number} is a Negative Number.")
+
+print("End of Program")
