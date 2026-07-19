650343bae0d2bfcbab2b63db447d19c3dee95a2a
VivekGoswami017 <vivekgoswamivg336278@gmail.com>
2026-07-19 09:55:51 +0530

    Create Arithmetic_Operators.py

---
+++ Python_All_practice_Code/Operators/Arithmetic_Operators.py
@@ -0,0 +1,11 @@
+Number1=int(input("Enter Number 1 Value :"))
+Number2=int(input("Enter Number 2 Value :"))
+
+#Arithmetic operator
+print(f"addition of {Number1} and {Number2} is--> {Number1+Number2}")            #Addition
+print(f"sub of {Number1} and {Number2} is--> {Number1-Number2}")                #Subtraction
+print(f"multi of {Number1} and {Number2} is--> {Number1*Number2}")               #Multiplication
+print(f"div of {Number1} and {Number2} is--> {Number1/Number2}")                 #Division
+print(f"power of {Number1} and {Number2} is--> {Number1**Number2}")              #Exponent(power)
+print(f"floor division of {Number1} and {Number2} is--> {Number1//Number2}")     #Floor division
+print(f"modul of {Number1} and {Number2} is--> {Number1%Number2}")               #Modulus(remindNumber)
