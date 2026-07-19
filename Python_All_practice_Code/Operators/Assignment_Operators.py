4af28f2204c262ef4fa90847a2c58b2157538a3e
VivekGoswami017 <vivekgoswamivg336278@gmail.com>
2026-07-19 09:55:56 +0530

    Create Assignment_Operators.py

---
+++ Python_All_practice_Code/Operators/Assignment_Operators.py
@@ -0,0 +1,19 @@
+Number=10
+
+Number += 5
+print(Number)  #Add and Assign Operator
+
+Number -= 5
+print(Number)  #Subtract and Assign OperaNumber
+
+Number *= 5
+print(Number)  #Multiply and Assign Operator
+
+Number /= 5
+print(Number)  #Divide and Assign Operator
+
+Number //= 5
+print(Number)  #Floor Divide and Assign Operator
+
+Number %= 5
+print(Number)  #Modulus and Assign Operator
