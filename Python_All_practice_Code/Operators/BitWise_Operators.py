1990497a1f4bb1b926633afe9bb0bb4c72a6e1cc
VivekGoswami017 <vivekgoswamivg336278@gmail.com>
2026-07-20 08:48:44 +0530

    Create BitWise_Operators.py

---
+++ Python_All_practice_Code/Operators/BitWise_Operators.py
@@ -0,0 +1,10 @@
+Number1=int(input("Enter Number 1 Value :"))
+Number2=int(input("Enter Number 2 Value :"))
+
+#Bitwise operator
+print(f"{Number1} & {Number2} is---> {Number1&Number2}")       #and
+print(f"{Number1} | {Number2} is---> {Number1|Number2}")       #or
+print(f"{Number1} ^ {Number2} is---> {Number1^Number2}")       #xor
+print(f"{Number1} ~ {Number2} is---> {~Number1}{~Number2}")    #not
+print(f"{Number1} << {Number2} is---> {Number1<<Number2}")     #left shift
+print(f"{Number1} >>{Number2} is---> {Number1>>Number2}")      #right shift
