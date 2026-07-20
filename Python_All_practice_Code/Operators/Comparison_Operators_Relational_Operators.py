3e9bb67d4b47c7bb494c430529e2208395ecba83
VivekGoswami017 <vivekgoswamivg336278@gmail.com>
2026-07-20 08:48:39 +0530

    Create Comparison_Operators_Relational_Operators.py

---
+++ Python_All_practice_Code/Operators/Comparison_Operators_Relational_Operators.py
@@ -0,0 +1,10 @@
+Number1=int(input("Enter Number 1 Value :"))
+Number2=int(input("Nnter Number 2 Value :"))
+
+#Relational or comparison operator
+print(f" {Number1} >  {Number2} is---> {Number1>Number2}")     #Grater than
+print(f" {Number1} <  {Number2} is---> {Number1<Number2}")     #Less than
+print(f" {Number1} !=  {Number2} is--->  {Number1!=Number2}")   #Not equal to
+print(f" {Number1} >= {Number2} is--->  {Number1>=Number2}")    #Greater than or equal to
+print(f" {Number1} <=  {Number2} is---> {Number1<=Number2}")   #Less than or equal to
+print(f" {Number1} ==  {Number2} is--->  {Number1==Number2}")   #Equal to
