ef3be559b52a38dfbbb72e9e65dcd47b0169975d
VivekGoswami017 <vivekgoswamivg336278@gmail.com>
2026-07-22 10:38:32 +0530

    Create Identity_Operator.py

---
+++ Python_All_practice_Code/Operators/Identity_Operator.py
@@ -0,0 +1,10 @@
+Student_Age =20
+Same_Age =Student_Age
+print(Student_Age is Same_Age)      # True
+print(Student_Age is not Same_Age)  # False
+
+
+list1 =[1, 2, 3]
+list2 =[1, 2, 3]
+print(list1 is list2)      # False
+print(list1 is not list2)  # True
