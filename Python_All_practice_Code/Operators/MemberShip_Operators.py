31cab158dcae17a93213d51971391d66bf61e1bf
VivekGoswami017 <vivekgoswamivg336278@gmail.com>
2026-07-22 10:38:28 +0530

    Create MemberShip_Operators.py

---
+++ Python_All_practice_Code/Operators/MemberShip_Operators.py
@@ -0,0 +1,11 @@
+Student_Name ="Vivek Goswami"
+print("Vivek" in Student_Name)      # True
+print("Rahul" not in Student_Name)  # True
+
+Mobile_Number = "9876543210"
+print("5" in Mobile_Number)         # True
+print("0" not in Mobile_Number)     # False
+
+Students = ["Vivek","Uday","Hitesh","Meet"]
+print("Hitesh" in Students)         # True
+print("Uday" not in Students)      # False
