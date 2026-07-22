c8821be14cbae3e4bfb628a8632bfc9b9bdfbfcc
VivekGoswami017 <vivekgoswamivg336278@gmail.com>
2026-07-22 10:38:38 +0530

    Create Logical_Operator.py

---
+++ Python_All_practice_Code/Operators/Logical_Operator.py
@@ -0,0 +1,9 @@
+
+Number=100
+print(Number>50 and Number<500) #Both Condition True Compulsory
+
+Number=100
+print(Number>50 or 1000<500)   #Only One Condition True Compulsory
+
+Number=100
+print(not(Number>60))     #True To False  And False To True convert
